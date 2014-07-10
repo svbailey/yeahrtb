41bad6052ebf7bbf3f3a3ac1e23e4d9594c64817
morningzhang <149151874@qq.com>
2014-07-10 17:50:18 +0800

    添加logger模块

--- src/rtb_http.js
+++ src/rtb_http.js
@@ -2,8 +2,12 @@
  * Created by mor on 14-7-10.
  */
 var http = require('http');
+var logger = require('./logger').logger('http_server');
+
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    logger.info("hello");
     res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
