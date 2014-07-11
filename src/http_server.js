0ba4531faddb1607c95e1de0f2fc803d59260f53
zhoushineyoung <zhoushineyoung@hotmail.com>
2014-07-11 18:13:40 +0800

    add json str "hello word" as response

--- src/http_server.js
+++ src/http_server.js
@@ -8,6 +8,8 @@ var logger = require('./logger').logger(__filename.split("//")[-1]);
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     logger.info("hello");
-    console.log("Hello World");
+//    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("{\"name\":\"Hello World\"}");
+    res.end();
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
