735dcc6def34fcf6f837e0431fb4454cc8fa0dd1
zhangliming <149151874@qq.com>
2014-07-16 18:10:57 +0800

    简单版本的res，用来调试

--- src/bid.js
+++ src/bid.js
@@ -2,45 +2,22 @@
  * Created by zhangliming on 14-7-16.
  */
 
-var bid_obj = {
-    "bidid": "",  //optional to DSP
-    "cur": "USD",
-    "id": "",
-    "seatbid": [
-        {
-            "seat": "",//optional to DSP, required if using multiple seats
-            "bid": [
-                {
-                    "id": "",
-                    "adid": "",
-                    "adm": "",
-                    "adomain": [""],
-                    "attr": [4],
-                    "cid": "",
-                    "crid": "",
-                    "crtype": "Image", //recommended
-                    "impid": "1",
-                    "iurl": "http://adserver.com/pathtosampleimage.jpg",
-                    "nurl": "",
-                    "price": 0.999,
-                    "ext":  //required for certain request types, not always passed
-                    {
-                        "data": [
-                            {
-                                "id": "",
-                                "name": "",
-                                "segment": [
-                                    {
-                                        "id": "17"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ] }
-    ]
-};
+var bid_obj = {"units": 0, "id": "", "seatbid": [
+    {
+        "bid": [
+            {
+                "iurl": "http://www.mopubbidderimageurl.com",
+                "cid": "afhjk234",
+                "crid": "ajfwep420",
+                "price": 24.0,
+                "adid": "adfo3btnt",
+                "adomain": "http://www.mopubbidder.com",
+                "adm": "<html><b>hiii</b></html>",
+                "impid": "eigu203f"
+            }
+        ]
+    }
+], "cur": "USD", "bidid": "dv09gn3k"}
 
 
 
